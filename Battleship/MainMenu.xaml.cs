ce5e23fc3c86afc00ca63445ab8038ab169af756
chaoukis <sammy.chaouki@dawsoncollege.qc.ca>
2017-12-05 01:41:05 -0500

    MainWindow Completed -- Add AUDIO

--- Battleship/MainMenu.xaml.cs
+++ Battleship/MainMenu.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,11 +20,41 @@ namespace Battleship
     /// </summary>
     public partial class MainMenu : Window
     {
+        private MediaPlayer player = new MediaPlayer();
         public MainMenu()
         {
             InitializeComponent();
         }
 
-        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            player.Open(new Uri("Audio/Strength of a Thousand Men - Two Steps from Hell.mp3"));
+
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+
+        private void Start_Game_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Start_Game_Btn.Visibility = Visibility.Hidden;
+            Difficulty_Txt.Visibility = Visibility.Visible;
+            EasyDifficulty_Btn.Visibility = Visibility.Visible;
+            MediumDifficulty_Btn.Visibility = Visibility.Visible;
+            HardDifficulty_Btn.Visibility = Visibility.Visible;
+        }
+
+        private void Quit_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Help_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("To start a game of Battleship, PRESS START. You will then be prompted to choose from 3 different levels of difficulty. Choose one " +
+                "and you will be able to play and enjoy our version of Battleship.");
+        }
     }
 }
